b8ff945fb5b919ebf2486a06c12020327774e265
Jamie Buckell <jamie.buckell@leighton.com>
2023-10-03 21:03:27 +0100

    Added sentry for error logging

--- src/main.js
+++ src/main.js
@@ -8,6 +8,30 @@ import { store } from "./store/index";
 import permissions from "./plugins/permissions";
 import moment from "moment";
 import VueSocialSharing from "vue-social-sharing";
+import * as Sentry from "@sentry/vue";
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    Vue,
+    dsn: "https://545dea5d3e63ea35e8b566787a6c2706@o4505988076011520.ingest.sentry.io/4505988079812608",
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: [
+          "localhost",
+          /^https:\/\/api\.cause-foundation\.org\.uk/,
+        ],
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      }),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 // plugin setup
 Vue.use(LightBootstrap);
