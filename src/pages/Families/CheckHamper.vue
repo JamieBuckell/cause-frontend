701e9bad5c0e4781b7a3e9cad6182114908296dc
Jamie Buckell <email@jamiebuckell.co.uk>
2024-09-08 16:07:32 +0100

    Index fix

--- src/pages/Families/CheckHamper.vue
+++ src/pages/Families/CheckHamper.vue
@@ -34,7 +34,11 @@
                   </qrcode-stream>
 
                   <br />
-                  <l-alert type="danger" v-for="m in messages" :key="m">
+                  <l-alert
+                    type="danger"
+                    v-for="(m, idx) in messages"
+                    :key="idx"
+                  >
                     <span> {{ getErrorMessage(m) }}</span>
                   </l-alert>
 
@@ -113,19 +117,19 @@
   </auth-layout>
 </template>
 <script>
-import Vue from 'vue'
-import { FadeRenderTransition } from 'src/components/index'
-import AuthLayout from 'src/pages/Dashboard/Pages/AuthLayout.vue'
-import { extend } from 'vee-validate'
-import { required, email, min } from 'vee-validate/dist/rules'
-import { getHamperOverview, getHamperFeedbackLabels } from '@/api/families.api'
-import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
-import LAlert from 'src/components/Alert'
-import Swal from 'sweetalert2'
+import Vue from "vue";
+import { FadeRenderTransition } from "src/components/index";
+import AuthLayout from "src/pages/Dashboard/Pages/AuthLayout.vue";
+import { extend } from "vee-validate";
+import { required, email, min } from "vee-validate/dist/rules";
+import { getHamperOverview, getHamperFeedbackLabels } from "@/api/families.api";
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
+import LAlert from "src/components/Alert";
+import Swal from "sweetalert2";
 
-extend('email', email)
-extend('required', required)
-extend('min', min)
+extend("email", email);
+extend("required", required);
+extend("min", min);
 
 export default {
   components: {
@@ -139,124 +143,127 @@ export default {
       messages: [],
       hamperDetails: {},
       submitting: false,
-      camera: 'auto',
+      camera: "auto",
       showScanConfirmation: false,
       maintenanceMode: false,
-      logo: '/static/img/cause-foundation-logo.png',
-      logoAlt: 'CAUSE Foundation Logo',
-      hamperId: '',
-      numberOfBags: '',
+      logo: "/static/img/cause-foundation-logo.png",
+      logoAlt: "CAUSE Foundation Logo",
+      hamperId: "",
+      numberOfBags: "",
       callback: false,
       showerr: false,
       resend: false,
       valid: false,
       emailrules: {
-        required: (value) => !!value || 'E-mail is required',
+        required: (value) => !!value || "E-mail is required",
         email: (value) => {
           const pattern =
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'E-mail must be valid'
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "E-mail must be valid";
         },
       },
       passRules: [
-        (v) => !!v || 'Password is required',
-        (v) => !v || v.length >= 8 || 'Password must be at least 8 characters',
+        (v) => !!v || "Password is required",
+        (v) => !v || v.length >= 8 || "Password must be at least 8 characters",
       ],
       hidepw: true,
       loader: false,
       loading: false,
-    }
+    };
   },
   methods: {
     async downloadFeedbackPDF() {
-      this.messages = []
-      this.hamperDetails.offset = 1000
-      const res = await getHamperFeedbackLabels({...this.hamperDetails, campaignId: this.$store.getters.getActiveCampaign})
+      this.messages = [];
+      this.hamperDetails.offset = 1000;
+      const res = await getHamperFeedbackLabels({
+        ...this.hamperDetails,
+        campaignId: this.$store.getters.getActiveCampaign,
+      });
       if (res.status == 200) {
-        const linkSource = `data:application/pdf;base64,${res.data}`
-        const downloadLink = document.createElement('a')
-        const fileName = `feedback-slips-pdf`
+        const linkSource = `data:application/pdf;base64,${res.data}`;
+        const downloadLink = document.createElement("a");
+        const fileName = `feedback-slips-pdf`;
 
-        downloadLink.href = linkSource
-        downloadLink.download = fileName
-        downloadLink.click()
-        this.downloadPending = false
+        downloadLink.href = linkSource;
+        downloadLink.download = fileName;
+        downloadLink.click();
+        this.downloadPending = false;
       } else {
         if (res?.data?.messages) {
           Swal.fire({
-            title: 'Error',
+            title: "Error",
             text: Object.keys(res?.data?.messages)
               .map((k) => res?.data?.messages[k])
               .join(),
             timer: 2000,
             showConfirmButton: false,
-          })
+          });
         }
       }
     },
     async checkHamperId() {
-      this.hamperId = this.hamperId.toUpperCase()
-      this.messages = []
-      const res = await getHamperOverview(this.hamperId)
+      this.hamperId = this.hamperId.toUpperCase();
+      this.messages = [];
+      const res = await getHamperOverview(this.hamperId);
 
       if (!res.data.success) {
         if (res?.data?.messages) {
           this.messages = Object.keys(res?.data?.messages).map((k) => ({
             error: res?.data?.messages[k],
-          }))
+          }));
         }
       } else {
-        this.hamperDetails = res?.data.hamper
+        this.hamperDetails = res?.data.hamper;
       }
 
-      this.showScanConfirmation = true
-      await this.timeout(2500)
-      this.showScanConfirmation = false
+      this.showScanConfirmation = true;
+      await this.timeout(2500);
+      this.showScanConfirmation = false;
     },
     async onInit(promise) {
       try {
-        await promise
+        await promise;
       } catch (e) {
         // console.error(e)
       } finally {
-        this.showScanConfirmation = this.camera === 'off'
+        this.showScanConfirmation = this.camera === "off";
       }
     },
 
     async onDecode(content) {
-      this.hamperId = content
-      await this.checkHamperId()
+      this.hamperId = content;
+      await this.checkHamperId();
 
-      this.pause()
-      await this.timeout(500)
-      this.unpause()
+      this.pause();
+      await this.timeout(500);
+      this.unpause();
     },
 
     unpause() {
-      this.camera = 'auto'
+      this.camera = "auto";
     },
 
     pause() {
-      this.camera = 'off'
+      this.camera = "off";
     },
 
     timeout(ms) {
       return new Promise((resolve) => {
-        window.setTimeout(resolve, ms)
-      })
+        window.setTimeout(resolve, ms);
+      });
     },
     getErrorMessage(m) {
       for (const [key, value] of Object.entries(m)) {
-        return `${value}`
+        return `${value}`;
       }
     },
   },
   mounted() {
-    if (!this.userInGroup('admin')) {
+    if (!this.userInGroup("admin")) {
       // this.$router.push('/')
     }
   },
-}
+};
 </script>
 <style lang="scss">
 .scan-confirmation {
